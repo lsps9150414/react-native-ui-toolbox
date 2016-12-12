f571f932979f7d222c67562c48c0c77256296558
Philip Chen <lsps9150414@gmail.com>
2016-12-13 01:39:42 +0800

    Refactor theme proptypes

--- src/Typography.js
+++ src/Typography.js
@@ -6,77 +6,13 @@ import {
   Text,
 } from 'react-native';
 
+import themeProptypes from './constants/themeProptypes';
 import { DEFAULT_COLORS } from './constants/colors';
-
-const fontStylePropTypes = PropTypes.oneOf(['normal', 'italic']);
-const fontWeightPropTypes = PropTypes.oneOf(['normal', 'bold', '100', '200', '300', '400', '500', '600', '700', '800', '900']);
-const textDecorationLinePropTypes = PropTypes.oneOf(['none', 'underline', 'line-through', 'underline line-through']);
-const textShadowOffsetPropTypes = PropTypes.shape({ width: PropTypes.number, height: PropTypes.number });
 
 const propTypes = {
   children: PropTypes.string.isRequired,
   style: Text.propTypes.style,
-  theme: PropTypes.shape({
-    // Color
-    color: PropTypes.string,
-    colorHeading: PropTypes.string,
-    colorTitle: PropTypes.string,
-    colorSubtitle: PropTypes.string,
-    colorText: PropTypes.string,
-    // Family
-    fontFamily: PropTypes.string,
-    fontFamilyHeading: PropTypes.string,
-    fontFamilyTitle: PropTypes.string,
-    fontFamilySubtitle: PropTypes.string,
-    fontFamilyText: PropTypes.string,
-    // Size
-    fontSizeBase: PropTypes.number,
-    fontSizeHeading: PropTypes.number,
-    fontSizeTitle: PropTypes.number,
-    fontSizeSubtitle: PropTypes.number,
-    fontSizeText: PropTypes.number,
-    // Style
-    fontStyle: fontStylePropTypes,
-    fontStyleHeading: fontStylePropTypes,
-    fontStyleTitle: fontStylePropTypes,
-    fontStyleSubtitle: fontStylePropTypes,
-    fontStyleText: fontStylePropTypes,
-    // Weight
-    fontWeight: fontWeightPropTypes,
-    fontWeightHeading: fontWeightPropTypes,
-    fontWeightTitle: fontWeightPropTypes,
-    fontWeightSubtitle: fontWeightPropTypes,
-    fontWeightText: fontWeightPropTypes,
-    // Line Height
-    lineHeightHeading: PropTypes.number,
-    lineHeightTitle: PropTypes.number,
-    lineHeightSubtitle: PropTypes.number,
-    lineHeightText: PropTypes.number,
-    // Text Decoration Line
-    textDecorationLine: textDecorationLinePropTypes,
-    textDecorationLineHeading: textDecorationLinePropTypes,
-    textDecorationLineTitle: textDecorationLinePropTypes,
-    textDecorationLineSubtitle: textDecorationLinePropTypes,
-    textDecorationLineText: textDecorationLinePropTypes,
-    // Shadow Color
-    textShadowColor: PropTypes.string,
-    textShadowColorHeading: PropTypes.string,
-    textShadowColorTitle: PropTypes.string,
-    textShadowColorSubtitle: PropTypes.string,
-    textShadowColorText: PropTypes.string,
-    // Shadow Offset
-    textShadowOffset: textShadowOffsetPropTypes,
-    textShadowOffsetHeading: textShadowOffsetPropTypes,
-    textShadowOffsetTitle: textShadowOffsetPropTypes,
-    textShadowOffsetSubtitle: textShadowOffsetPropTypes,
-    textShadowOffsetText: textShadowOffsetPropTypes,
-    // Shadow Radius
-    textShadowRadius: PropTypes.number,
-    textShadowRadiusHeading: PropTypes.number,
-    textShadowRadiusTitle: PropTypes.number,
-    textShadowRadiusSubtitle: PropTypes.number,
-    textShadowRadiusText: PropTypes.number,
-  }),
+  theme: themeProptypes,
 };
 
 const defaultProps = {};
---
+++ src/constants/themeProptypes.js
@@ -0,0 +1,71 @@
+import { PropTypes } from 'react';
+
+/* eslint-disable max-len */
+const fontStylePropTypes = PropTypes.oneOf(['normal', 'italic']);
+const fontWeightPropTypes = PropTypes.oneOf(['normal', 'bold', '100', '200', '300', '400', '500', '600', '700', '800', '900']);
+const textDecorationLinePropTypes = PropTypes.oneOf(['none', 'underline', 'line-through', 'underline line-through']);
+const textShadowOffsetPropTypes = PropTypes.shape({ width: PropTypes.number, height: PropTypes.number });
+/* eslint-enable max-len */
+
+export default PropTypes.shape({
+  /* TEXT */
+  // Color
+  color: PropTypes.string,
+  colorHeading: PropTypes.string,
+  colorTitle: PropTypes.string,
+  colorSubtitle: PropTypes.string,
+  colorText: PropTypes.string,
+  // Family
+  fontFamily: PropTypes.string,
+  fontFamilyHeading: PropTypes.string,
+  fontFamilyTitle: PropTypes.string,
+  fontFamilySubtitle: PropTypes.string,
+  fontFamilyText: PropTypes.string,
+  // Size
+  fontSizeBase: PropTypes.number,
+  fontSizeHeading: PropTypes.number,
+  fontSizeTitle: PropTypes.number,
+  fontSizeSubtitle: PropTypes.number,
+  fontSizeText: PropTypes.number,
+  // Style
+  fontStyle: fontStylePropTypes,
+  fontStyleHeading: fontStylePropTypes,
+  fontStyleTitle: fontStylePropTypes,
+  fontStyleSubtitle: fontStylePropTypes,
+  fontStyleText: fontStylePropTypes,
+  // Weight
+  fontWeight: fontWeightPropTypes,
+  fontWeightHeading: fontWeightPropTypes,
+  fontWeightTitle: fontWeightPropTypes,
+  fontWeightSubtitle: fontWeightPropTypes,
+  fontWeightText: fontWeightPropTypes,
+  // Line Height
+  lineHeightHeading: PropTypes.number,
+  lineHeightTitle: PropTypes.number,
+  lineHeightSubtitle: PropTypes.number,
+  lineHeightText: PropTypes.number,
+  // Text Decoration Line
+  textDecorationLine: textDecorationLinePropTypes,
+  textDecorationLineHeading: textDecorationLinePropTypes,
+  textDecorationLineTitle: textDecorationLinePropTypes,
+  textDecorationLineSubtitle: textDecorationLinePropTypes,
+  textDecorationLineText: textDecorationLinePropTypes,
+  // Shadow Color
+  textShadowColor: PropTypes.string,
+  textShadowColorHeading: PropTypes.string,
+  textShadowColorTitle: PropTypes.string,
+  textShadowColorSubtitle: PropTypes.string,
+  textShadowColorText: PropTypes.string,
+  // Shadow Offset
+  textShadowOffset: textShadowOffsetPropTypes,
+  textShadowOffsetHeading: textShadowOffsetPropTypes,
+  textShadowOffsetTitle: textShadowOffsetPropTypes,
+  textShadowOffsetSubtitle: textShadowOffsetPropTypes,
+  textShadowOffsetText: textShadowOffsetPropTypes,
+  // Shadow Radius
+  textShadowRadius: PropTypes.number,
+  textShadowRadiusHeading: PropTypes.number,
+  textShadowRadiusTitle: PropTypes.number,
+  textShadowRadiusSubtitle: PropTypes.number,
+  textShadowRadiusText: PropTypes.number,
+});

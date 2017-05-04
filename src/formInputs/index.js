b0497760bd46bc9205ad1d22380ebd8b416c387b
Philip Chen <lsps9150414@gmail.com>
2017-05-05 01:36:22 +0800

    Export FormSelect

--- src/formInputs/index.js
+++ src/formInputs/index.js
@@ -1,9 +1,11 @@
 import _FormDatePicker from './FormDatePicker';
 import _FormInputHOC from './FormInputHOC';
 import _FormPicker from './FormPicker';
+import _FormSelect from './FormSelect';
 import _FormTextInput from './FormTextInput';
 
 export const FormInputHOC = _FormInputHOC;
 export const FormDatePicker = _FormInputHOC(_FormDatePicker);
 export const FormPicker = _FormInputHOC(_FormPicker);
+export const FormSelect = _FormInputHOC(_FormSelect);
 export const FormTextInput = _FormInputHOC(_FormTextInput);

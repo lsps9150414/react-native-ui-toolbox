79934fa5443134b997c79cd3c3c057448df05b73
Philip Chen <lsps9150414@gmail.com>
2018-01-19 03:23:30 +0800

    update dependencies

--- src/Avatar.js
+++ src/Avatar.js
@@ -1,7 +1,6 @@
 import _ from 'lodash';
-import React, {
-  PropTypes,
-} from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 import { Icon } from 'react-native-elements';
 import {
   Image,
--- src/constants/themeProptypes.js
+++ src/constants/themeProptypes.js
@@ -1,4 +1,4 @@
-import { PropTypes } from 'react';
+import { PropTypes } from 'prop-types';
 
 /* eslint-disable max-len */
 const fontStylePropTypes = PropTypes.oneOf(['normal', 'italic']);
